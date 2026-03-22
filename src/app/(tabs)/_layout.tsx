7c94cd975b31e3be9694a98408fa4c123ac989ea
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2026-03-22 18:05:58 +0200

    fix: :art: remove tab bar

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -7,7 +7,7 @@ export default function TabsLayout() {
             <Tabs.Screen
                 name="index"
                 options={{
-                    title: "Home",
+                    headerShown: false,
                     tabBarIcon: ({ color, focused, size }) => (
                         <Ionicons
                             name={(focused && "home") || "home-outline"}
@@ -20,7 +20,7 @@ export default function TabsLayout() {
             <Tabs.Screen
                 name="profile"
                 options={{
-                    title: "Profile",
+                    headerShown: false,
                     tabBarIcon: ({ color, focused, size }) => (
                         <Ionicons
                             name={(focused && "person") || "person-outline"}
